1bf1e0023e13daa63524fded6992b4de4e97ef65
Wallies21 <waldoackerman@gmail.com>
2020-10-28 12:20:01 +0200

    DisplayName

--- Project_2_Dimension_Data_Collab/Project_2_Dimension_Data_Collab/Models/Cmpg323Project2Dataset.cs
+++ Project_2_Dimension_Data_Collab/Project_2_Dimension_Data_Collab/Models/Cmpg323Project2Dataset.cs
@@ -7,40 +7,75 @@ namespace Project_2_Dimension_Data_Collab.Models
     public partial class Cmpg323Project2Dataset
     {
         public string Age { get; set; } 
-        public string Attrition { get; set; } 
-        public string BusinessTravel { get; set; }
+        public string Attrition { get; set; }
         [DisplayName("Business Travel")]
+        public string BusinessTravel { get; set; }
+        [DisplayName("Daily Rate")]
         public string DailyRate { get; set; }
+        [DisplayName("Department")]
         public string Department { get; set; }
+        [DisplayName("Distance From Home")]
         public string DistanceFromHome { get; set; }
         public string Education { get; set; }
+        [DisplayName("Education Field")]
         public string EducationField { get; set; }
+        [DisplayName("Employee Count")]
         public string EmployeeCount { get; set; }
+        [DisplayName("Employee Number")]
         public string EmployeeNumber { get; set; }
+        [DisplayName("Environment Satisfaction")]
         public string EnvironmentSatisfaction { get; set; }
         public string Gender { get; set; }
+        [DisplayName("Hourly Rate")]
         public string HourlyRate { get; set; }
+        [DisplayName("Job Involvement")]
         public string JobInvolvement { get; set; }
+        [DisplayName("Job Level")]
         public string JobLevel { get; set; }
+        [DisplayName("Job Role")]
         public string JobRole { get; set; }
+        [DisplayName("Job Satisfaction")]
         public string JobSatisfaction { get; set; }
+        [DisplayName("Marital Status")]
         public string MaritalStatus { get; set; }
+        [DisplayName("Monthly Income")]
         public string MonthlyIncome { get; set; }
+        [DisplayName("Monthly Rate")]
         public string MonthlyRate { get; set; }
+        [DisplayName("Number of Companies Worked At")]
         public string NumCompaniesWorked { get; set; }
+        [DisplayName("Over 18")]
         public string Over18 { get; set; }
+        [DisplayName("Over Time")]
         public string OverTime { get; set; }
+        [DisplayName("Percent Salary Hike")]
         public string PercentSalaryHike { get; set; }
+        [DisplayName("Performance Rating")]
         public string PerformanceRating { get; set; }
+        [DisplayName("Relationship Satisfaction")]
         public string RelationshipSatisfaction { get; set; }
+        [DisplayName("Standard Hours")]
         public string StandardHours { get; set; }
+        [DisplayName("Stock Option Level")]
         public string StockOptionLevel { get; set; }
+        [DisplayName("Total Working Years")]
         public string TotalWorkingYears { get; set; }
+        [DisplayName("Training Times Last Year")]
         public string TrainingTimesLastYear { get; set; }
+        [DisplayName("Work/Life Balance")]
         public string WorkLifeBalance { get; set; }
+        [DisplayName("Years At Company")]
         public string YearsAtCompany { get; set; }
+        [DisplayName("Years In Current Role")]
         public string YearsInCurrentRole { get; set; }
+        [DisplayName("Years Since Last Promotion")]
         public string YearsSinceLastPromotion { get; set; }
+        [DisplayName("Years With Current Manager")]
         public string YearsWithCurrManager { get; set; }
+        
+
+
+
+
     }
 }
